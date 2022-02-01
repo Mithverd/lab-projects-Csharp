9e9ce0329ce2737c7b57976d50daec7317130c5d
Erdem Aksoy <79666268+Mithverd@users.noreply.github.com>
2022-02-01 23:25:33 +0300

    there is all lesson notes

---
+++ extra exam examples/prjYLHafta7/prjYLHafta7/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace prjYLHafta7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPersoneller = new System.Windows.Forms.ListBox();
+            this.btnDosyayiOku = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPersoneller
+            // 
+            this.lbPersoneller.FormattingEnabled = true;
+            this.lbPersoneller.Location = new System.Drawing.Point(12, 12);
+            this.lbPersoneller.Name = "lbPersoneller";
+            this.lbPersoneller.Size = new System.Drawing.Size(570, 251);
+            this.lbPersoneller.TabIndex = 0;
+            // 
+            // btnDosyayiOku
+            // 
+            this.btnDosyayiOku.Location = new System.Drawing.Point(12, 269);
+            this.btnDosyayiOku.Name = "btnDosyayiOku";
+            this.btnDosyayiOku.Size = new System.Drawing.Size(75, 23);
+            this.btnDosyayiOku.TabIndex = 1;
+            this.btnDosyayiOku.Text = "Dosyayi Oku";
+            this.btnDosyayiOku.UseVisualStyleBackColor = true;
+            this.btnDosyayiOku.Click += new System.EventHandler(this.btnDosyayiOku_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 303);
+            this.Controls.Add(this.btnDosyayiOku);
+            this.Controls.Add(this.lbPersoneller);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbPersoneller;
+        private System.Windows.Forms.Button btnDosyayiOku;
+    }
+}
+
---
+++ extra exam examples/prjYLHafta7/prjYLHafta7/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prjYLHafta7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDosyayiOku_Click(object sender, EventArgs e)
+        {
+            string sSatir = "";
+            string[] sDegerler;
+            FileStream fs = new FileStream("personeller.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            clsPersonel personel;
+            while (!sr.EndOfStream)
+            {
+                sSatir = sr.ReadLine();
+                sDegerler = sSatir.Split(',');
+
+                if (sDegerler.Length < 4)
+                    continue;
+
+                personel = new clsPersonel();
+                personel.sAdi = sDegerler[0];
+                personel.iCalismaSuresi = int.Parse(sDegerler[1]);
+                personel.sResimYolu = sDegerler[2];
+                personel.dMaasi = double.Parse(sDegerler[3]);
+
+                lbPersoneller.Items.Add(personel.Yazdir());
+            }
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
